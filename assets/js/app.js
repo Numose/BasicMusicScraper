5e5d2467499128431afe28c5b85e9147b9c5c170
Numose <andrew.cernek@gmail.com>
2016-10-17 16:49:29 -0700

    get html fetcher working, traverse dom for audio tags, return as json collection

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,35 @@
 var app = {
-    init: function() {
-    }
+	init: function() {
+		_.templateSettings.variable = 'track'; 
+			var template = _.template(
+			$('script.template').html()
+		);
+	},
+	insertExample: function(url) {
+		$('input#input_url').val(url);
+	},
+	renderTracks: function(data) {
+		_.each(data, function(elem) { $('#tracks').prepend(template(elem)); });
+	},
+	getHtml: function(url) {
+		$.ajax({
+			url: '/scrape',
+			type: 'GET',
+			data: {url: url},
+			success: function(data) {
+				this.renderTracks(data);
+			},
+			error: function(err) {
+				console.error(err);
+			}
+		});
+	}
 };
+
+// // // event listeners // // //
+
+$('form#form_new_scrape').on('submit', function(e){
+	e.preventDefault();
+	var url = $('input#input_url').val();
+	url !== null ? app.getHtml(url) : console.error('please enter a url');
+});
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const request = require('request');
+const cheerio = require('cheerio');
 const port = 8080;
 
 app.get('/', function(req, res) {
@@ -15,6 +16,37 @@ app.get('/scripts', function(req, res) {
 
 app.get('/styles', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/css/styles.css'));
+});
+
+app.get('/scrape', function(req, res) {
+
+  request(req.query.url, function(error, response, html) {
+    if (!error) {
+
+      var tracks = [];
+
+      // temp data
+      tracks = [
+        {"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_Event_DiabloAnniversary_TristramGuitar (Guitar)_01.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_Event_DiabloAnniversary_TristramGuitar (Guitar)_02.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_Event_DiabloAnniversary_TristramGuitar (Guitar)_03.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_Event_DiabloAnniversary_TristramGuitar (Guitar)_04.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_Event_DiabloAnniversary_TristramGuitar (Orchestra)_01.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_Event_DiabloAnniversary_TristramGuitar (Orchestra)_02.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_Event_DiabloAnniversary_TristramGuitar (Orchestra)_03.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_Event_DiabloAnniversary_TristramGuitar (Orchestra)_04.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_Event_DiabloAnniversary_TristramGuitar (Orchestra)_05.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_KarazhanGameMansHall_Walk_01.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_KarazhanGameMansHall_Walk_02.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_KarazhanGameMansHall_Walk_03.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_KarazhanManaDevourer_Walk_02.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_KarazhanManaDevourer_Walk_03.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_71_KarazhanVizaduum_Intro.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_CityWalk_Ironforge_06.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_CityWalk_Org_01.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_CityWalk_Org_03.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_CityWalk_Org_04.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_CityWalk_Org_05.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_CityWalk_Org_06.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_CityWalk_Org_07.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_CityWalk_Org_10.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_GreenchWalk_01_01.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_GreenchWalk_01_02.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_GreenchWalk_01_03.mp3"},{"url":"http://media.mmo-champion.com/images/news/2016/september/music/MUS_WinterVeil_GreenchWalk_01_04.mp3"}
+      ];
+      
+      var $ = cheerio.load(html);
+
+      $('body').find('audio').each(function(idx, elem) {
+        // console.log('==============');
+        // console.log(elem);
+        // console.log('==============');
+        tracks.push({url: elem.attribs.src});
+      });
+
+      // res.status(200).send('maybe it worked?');
+
+      res.json(tracks);
+    
+    } else {
+      console.log('Error when requesting html', error);
+    }
+  });
 });
 
 // app.get('/downloads', function(req, res) {
