6b85274550dea34c8591d1205c2c130fef1c50a8
Numose <andrew.cernek@gmail.com>
2016-10-17 12:35:43 -0700

    gulp installed

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ var app = express();
 var path = require('path');
 var port = 8080;
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/client/index.html'));
+});
+
+app.get('/scripts', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/js/scripts.min.js'));
+});
+
+app.get('/styles', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/css/styles.css'));
 });
 
 app.listen(port, function () {
